f9deb6230b4f3418761c37fca346cb8d98ed5c2e
unknown <reo3@uchicago.edu>
2022-04-10 22:15:43 -0400

    Updated formatting

--- Sample-Code-Geospatial-Mapping.py
+++ Sample-Code-Geospatial-Mapping.py
@@ -4,9 +4,7 @@
 # "Investigating Freshwater Salinity and Economic Development"
 
 def world_pd_2_world_geo(df_final):
-    """
-    Takes a pandas df, merges with naturalearth_lowres, returns a geopandas df.
-    """
+    # Takes a pandas df, merges with naturalearth_lowres, returns a geopandas df.
     world_geo = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
     world_geo['name'] = world_geo['name'].apply(lambda x : reconcile_world_country_names(x, df_final))
     df_merged_pandas = df_final.merge(world_geo[['name', 'geometry']], left_on='Country', right_on='name')
@@ -23,14 +21,10 @@ def plot_grand_summary_world_map(df_final, summary_var, save_title=False):
     fig_total, ax_total = plt.subplots(figsize=(7,7))
     divider = make_axes_locatable(ax_total) 
     cax = divider.append_axes('right', size='5%', pad=0.1) # To adjust map scale
-    ax_total = df_grand_total.plot(ax=ax_total,
-                                   column=summary_var,
-                                   edgecolor='grey',
-                                   legend=True,
-                                   cmap='Spectral',
-                                   cax=cax)
-    ax_total.set_title(f'Total {summary_var} Across All Data and Years', 
-                       weight='bold')
+    ax_total = df_grand_total.plot(ax=ax_total, column=summary_var,
+                                   edgecolor='grey', legend=True,
+                                   cmap='Spectral', cax=cax)
+    ax_total.set_title(f'Total {summary_var} Across All Data and Years', weight='bold')
     if(save_title):
         fig_total.savefig(os.path.join(path,f'{save_title}.png'))
 
@@ -47,15 +41,11 @@ def plot_2_by_2_choropleth(df_final, year, var_list, save_title=False):
     for this_ax, var in zip(axs_flat, var_list):
         divider = make_axes_locatable(this_ax)
         cax = divider.append_axes('right', size='5%', pad=0.1)
-        this_ax = this_df.plot(ax=this_ax,
-                                  column=var,
-                                  edgecolor='black',
-                                  legend=True,
-                                  cmap=color_map_dict[var],
-                                  cax=cax)
+        this_ax = this_df.plot(ax=this_ax, column=var,
+                               edgecolor='black', legend=True,
+                               cmap=color_map_dict[var], cax=cax)
         var_title = var.replace('_',' ')
         this_ax.set_title(f'{var_title} in {year}', weight='bold')
         this_ax.set_facecolor(face_color)
     if(save_title):
-        fig_chlor.savefig(os.path.join(path, f'{save_title}.png'),
-                          bbox_inches='tight')
+        fig_chlor.savefig(os.path.join(path, f'{save_title}.png'), bbox_inches='tight')
